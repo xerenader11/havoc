b9f69a15ad9822dec4ac7a7ba2639cbf940ade7b
Jason Burgos <xerenader@gmail.com>
2013-06-07 08:00:50 +0800

    Separate header and footer. about.php

---
+++ application/views/footer.php
@@ -0,0 +1,56 @@
+  <footer id='footer'>
+    <div class='pull-right' style='text-align: right;'>
+      <div class='footer-copyright'>&copy; 2011-2013 Mineski, Inc.</div>
+    </div>
+
+    <nav>
+      <ul>
+        <li>
+          <a href="/about">About Mineski</a>
+        </li>
+        <li>
+          <a href="mailto:support@mineski.net">Contact</a>
+        </li>
+        <li>
+          <a href="/privacy">Privacy</a>
+        </li>
+        <li>
+          <a href="/terms">Terms</a>
+        </li>
+        <li>
+          <a href='http://infinity.mineski.net' target='_blank'>Mineski Infinity</a>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+
+
+    <script>
+      //<![CDATA[
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-12995712-1']);
+        _gaq.push(['_setDomainName', '.mineski.net']);
+        _gaq.push(['_trackPageview']);
+        
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      //]]>
+    </script>
+
+    <script type="text/javascript">if (!NREUMQ.f) { NREUMQ.f=function() {
+    NREUMQ.push(["load",new Date().getTime()]);
+    var e=document.createElement("script");
+    e.type="text/javascript";
+    e.src=(("http:"===document.location.protocol)?"http:":"https:") + "//" +
+      "d1ros97qkrwjf5.cloudfront.net/42/eum/rum.js";
+    document.body.appendChild(e);
+    if(NREUMQ.a)NREUMQ.a();
+    };
+    NREUMQ.a=window.onload;window.onload=NREUMQ.f;
+    };
+    NREUMQ.push(["nrfj","beacon-4.newrelic.com","7128fb1369","1529876","d15bEBRbWghcFBsKWVlUGgUEW0MQ",0,235,new Date().getTime(),"","66c678340b04057a","","",""]);</script>
+  </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html xmlns:og='http://ogp.me/ns#'>
+	<head>
+		<script type="text/javascript">var NREUMQ=NREUMQ||[];NREUMQ.push(["mark","firstbyte",new Date().getTime()]);
+		</script>
+		<title>About - Havoc</title>
+		<link href="//cdn.mineski.s3.amazonaws.com/assets/application-0c4b232e7ed08ed38843d55fa92febf6.css" media="all" rel="stylesheet" type="text/css" />
+		<script src="//cdn.mineski.s3.amazonaws.com/assets/application-63020e4f3536a6f1b2020ecd1b6b06d2.js" type="text/javascript"></script>
+		<link href="//cdn.mineski.s3.amazonaws.com/assets/favicon-3805169ccb7c3e64ed2da1ceb9b66443.png" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+	</head>
+
+	<body class='about' id='home'>
+		<div id='fb-root'>
+			<script>
+			  //<![CDATA[
+			    (function(d, s, id) {
+			      var js, fjs = d.getElementsByTagName(s)[0];
+			      if (d.getElementById(id)) return;
+			      js = d.createElement(s); js.id = id;
+			      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=123052287777145";
+			      fjs.parentNode.insertBefore(js, fjs);
+			    }(document, 'script', 'facebook-jssdk'));
+			  //]]>
+			</script>
+		</div>
+
+		<header id='header'>
+			<div class='right-half'>
+				<div id='top-banner'>
+					<a href='http://www.facebook.com/pages/GIGABYTE-Philippines/119382288144521' onClick="_gaq.push(['_trackEvent', 'Advertisements', 'Ad Click', 'Gigabyte Header Banner']);" rel='nofollow' style='margin:0;height:60px;display:block;' target='_blank'>
+						<img alt='ASUS' src='http://cdn.mineski.net/images/vendor/gigabyte-468x60.gif'>
+					</a>
+				</div>
+			</div>
+
+			<h1 id='logo'><a href="/">Mineski.net</a></h1>
+		</header>
+
+<div id='wrapper'>
+	<div id='inner-wrap'>
+		<div id='zenith'>
+			<div class='inner'>
+				<div class='clearfix'>
+					<div id='site-stats'>
+						<span class='divider'>&#124;</span>
+						<span class='emphasis'>Sat, 18 May 08:18 PHT</span>
+					</div>
+				
+					<nav id='core-nav'>
+						<ul>
+							<li>
+								<a href="/users/sign_in" class="">Login</a>
+							</li>
+							<li>
+								<a href="/users/sign_up" class="">Register</a>
+							</li>
+							<li>
+								<a href="/users/auth/facebook">Sign in with Facebook</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+
+	<aside id='directory'>
+		<div class='directory-inner'>
+			<ul class='pull-left' id='tags-nav'>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/index.php/plans">Plans and Programs</a>
+				</li>
+				<li>
+					<a href="/index.php/financial">Financial Statement</a>
+				</li>
+				<li>
+					<a href="/index.php/coreValues">Core Values</a>
+				</li>
+				<li>
+					<a href="/index.php/gallery">Gallery</a>
+				</li>
+				<li class="active">
+					<a href="/index.php/about">About</a>
+				</li>
+			</ul>
+
+			<div class='social-media'>
+				<a href="http://www.facebook.com/mineskidotnet" class="facebook" rel="nofollow" target="_blank" title="Like us on Facebook!"></a>
+				<a href="http://twitter.com/mineski" class="twitter" rel="nofollow" target="_blank" title="Follow us on Twitter!"></a>
+			</div>
+
+			<div id='cse-search-box'>
+				<form action='/search' class='pull-left'>
+					<div>
+						<input name='cx' type='hidden' value='001171155721843178657:refk_nrtxoy'>
+						<input name='cof' type='hidden' value='FORID:11'>
+						<input name='ie' type='hidden' value='UTF-8'>
+						<input class='search-query' name='q' placeholder='Search Havoc...' size='25' type='text'>
+						<script src='http://www.google.com/cse/brand?form=cse-search-box&amp;lang=en' type='text/javascript'></script>
+						</input>
+						</input>
+						</input>
+						</input>
+					</div>
+				</form>
+			</div>
+		</div>
+	</aside>
