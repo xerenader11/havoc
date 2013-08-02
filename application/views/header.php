4c33b73ab5f883b213c87737296b1ac72c3c0212
Mar Jay <marjay369@yahoo.com>
2013-08-02 16:48:11 +0800

    Changes in signin

--- application/views/header.php
+++ application/views/header.php
@@ -2,12 +2,12 @@
 <title>Truly Simple</title>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<link rel="stylesheet" type="text/css" href="/public/css/style.css" media="screen" />
-<link href="/public/jquery-ui/nyroModal/styles/nyroModal.css" media="all" rel="stylesheet" type="text/css" />
-<script src="/public/jquery-ui/nyroModal/js/jquery.nyroModal.custom.min.js" type="text/javascript"></script>
-<script type="text/javascript" language="javascript" src="/public/DataTables-1.9.4/media/js/jquery.dataTables.min.js"></script>
-<script src="/public/lightbox/js/lightbox-2.6.min.js"></script>
-<script src="/public/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+<link rel="stylesheet" type="text/css" href="/havoc/public/css/style.css" media="screen" />
+<link href="/havoc/public/jquery-ui/nyroModal/styles/nyroModal.css" media="all" rel="stylesheet" type="text/css" />
+<script src="/havoc/public/jquery-ui/nyroModal/js/jquery.nyroModal.custom.min.js" type="text/javascript"></script>
+<script type="text/javascript" language="javascript" src="/havoc/public/DataTables-1.9.4/media/js/jquery.dataTables.min.js"></script>
+<script src="/havoc/public/lightbox/js/lightbox-2.6.min.js"></script>
+<script src="/havoc/public/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
 
 <script type="text/javascript">
 		$(function() {
@@ -23,10 +23,10 @@
 </script>
 
 <style type="text/css" title="currentStyle">
-			@import "/public/DataTables-1.9.4/media/css/demo_page.css";
-			@import "/public/DataTables-1.9.4/media/css/demo_table.css";
-			@import "/public/lightbox/css/lightbox.css";
-			@import "/public/css/prettyPhoto.css";
+			@import "/havoc/public/DataTables-1.9.4/media/css/demo_page.css";
+			@import "/havoc/public/DataTables-1.9.4/media/css/demo_table.css";
+			@import "/havoc/public/lightbox/css/lightbox.css";
+			@import "/havoc/public/css/prettyPhoto.css";
 </style>
 
 </head>
@@ -34,6 +34,29 @@
 <div id="wrap">
   <div id="top">
     <h2> <a href="http://localhost/trulysimple/">HAVOC</a></h2>
+	 <div id="signin">
+	 
+  		<?php if(!$this->session->userdata('email')){ ?>
+		
+							
+								<td>
+									<a href="/havoc/index.php/signIn" class="">Sign In</a>
+								</td>
+								<td>
+									<a href="/havoc/index.php/signUp" class="">Register</a>
+								</td>
+							
+						<?php }else{ ?>
+							
+								<td>
+									<a href="/havoc/index.php/settings">Welcome, <font color="red"><?php echo $this->session->userdata('fname') . " " . $this->session->userdata('lname'); ?></font></a>
+								</td>
+								<td>
+									<a href="/havoc/index.php/signOut" class="">Sign Out</a>
+								</td>
+						
+					<?php } ?>
+  </div>
     <div id="menu">
       <ul>
         <li><a href="/" class="current">HOME</a></li>
@@ -47,26 +70,6 @@
 		<li><a href="/index.php/about">ABOUT</a></li>
       </ul>
     </div>
-  </div>
-  <div id="salutation">
-  		<?php if(!$this->session->userdata('email')){ ?>
-							<ul>
-								<li>
-									<a href="/index.php/signIn" class="">Sign In</a>
-								</li>
-								<li>
-									<a href="/index.php/signUp" class="">Register</a>
-								</li>
-							</ul>
-						<?php }else{ ?>
-							<ul>
-								<li>
-									<a href="/index.php/settings">Welcome, <font color="red"><?php echo $this->session->userdata('fname') . " " . $this->session->userdata('lname'); ?></font></a>
-								</li>
-								<li>
-									<a href="/index.php/signOut" class="">Sign Out</a>
-								</li>
-							</ul>
-						<?php } ?>
-  </div>
-
+  
+ 
+</div>
